e09bdfecb3795d1d9737f5d031638d37fb629d29
Hans Vos <hans.vos@codeimmersives.com>
2019-10-02 15:16:11 -0400

    last commit

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
-function isHot() {
-
+function isHot(temp) {
+  let temp1 = (temp >= 80) ? "Yes, it is indeed hot." : "No, it is not hot.";
+  return temp1;
 }
 
 
-function helloThere() {
-
+function helloThere(name) {
+  let name1 = (name.length < 6) ? `Hello, ${name}` : `Hi, ${name}`;
+  return name1;
 }
 
 
-function goodbyeYou() {
-
+function goodbyeYou(name) {
+  let name1 = (typeof(name) === 'string') ? `Goodbye, ${name}` : `Goodbye, stranger`;
+  return name1;
 }
+
 
 
 module.exports = {
